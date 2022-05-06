101a8a12100b0530d38cf221dcdf97a9773539b8
leonardob92 <74712035+leonardob92@users.noreply.github.com>
2022-05-06 14:42:22 +0100

    Add files via upload

--- Cluster_Aarhus_ParallelComputing/decoding.m
+++ Cluster_Aarhus_ParallelComputing/decoding.m
@@ -23,15 +23,19 @@ numperm = S.numperm;
 kfold = S.kfold;
 savedir = S.savedir;
 ii = S.ii;
-
+pairl = S.pairl;
 
 %pairwise decoding
-d = sll_decodesvm_pairwise_haufe(data,condid,'numpermutation',numperm,'verbose',2,'kfold',kfold); 
+if pairl == 1
+    d = sll_decodesvm_pairwise_haufe(data,condid,'numpermutation',numperm,'verbose',2,'kfold',kfold); %method = pairwise
+else
+    d = sll_decodesvm_pairwise_haufe(data,condid,'numpermutation',numperm,'method','multiclass','verbose',2,'kfold',kfold); %to get confusion matrix
+end
 save([savedir '/PD_SUBJ' num2str(ii) '.mat'],'d');
 
 %temporal generalization
-d = sll_decodesvm(data,condid,'numpermutation',numperm,'method','temporalgen','verbose',1,'kfold',kfold); %max correlation classifier
-save([savedir '/TG_SUBJ' num2str(ii) '.mat'],'d');
+% d = sll_decodesvm(data,condid,'numpermutation',numperm,'method','temporalgen','verbose',1,'kfold',kfold); %max correlation classifier
+% save([savedir '/TG_SUBJ' num2str(ii) '.mat'],'d');
 
 
 end
