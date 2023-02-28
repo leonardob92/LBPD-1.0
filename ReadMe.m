9765b4749a1d2effb427283c1e9da60bb4cbf9a2
leonardob92 <74712035+leonardob92@users.noreply.github.com>
2023-02-28 16:37:53 +0100

    Update ReadMe.m

--- ReadMe.m
+++ ReadMe.m
@@ -157,6 +157,7 @@
 % 44)FromCoordMatrix_2_3DNifti_8mm_LBPD_D
 % 45)From3DNifti_OrMNICoords_2_CoordMatrix_8mm_LBPD_D
 % 46)InducedResponses_Morlet_WholeBrain_LBPD_D
+% 47)Extract_MEGSensor_Information_LBPD_D
 
 % Additional scripts:
 % - Workbench_Codes_LocalMac_Example
